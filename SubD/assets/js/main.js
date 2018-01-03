014a4b637725fa92e27c4729b8de769c372ca56d
echo000 <08finncam@gmail.com>
2018-01-03 20:53:53 +0000

    Yet more (I'll eventually get around to adding text)

--- SubD/assets/js/main.js
+++ SubD/assets/js/main.js
@@ -55,8 +55,8 @@ jQuery(document).ready(function($) {
     );
 
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "IonicaBizau");
-    
+    GitHubCalendar("#github-graph", "echo000");
+
 
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
     GitHubActivity.feed({ username: "echo000", selector: "#ghfeed" });
